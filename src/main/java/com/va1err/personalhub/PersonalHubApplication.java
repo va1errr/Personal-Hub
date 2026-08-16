71d16923d43a83a2f71636efa43501a5aa3b4555
va1errr <valeragladyshev2@gmail.com>
2026-08-16 23:10:12 +0300

    fix: environment variables + wrong package

--- src/main/java/com/va1err/personalhub/PersonalHubApplication.java
+++ src/main/java/com/va1err/personalhub/PersonalHubApplication.java
@@ -1,4 +1,4 @@
-package main.java.com.va1err.personalhub;
+package com.va1err.personalhub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
